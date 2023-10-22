5eccc352163ad215932564e294c183760c752b93
gautam8387 <goutamahuja8387@gmail.com>
2023-10-23 00:11:27 +0530

    A3 V1

---
+++ Assignment-3/3_A3.c
@@ -0,0 +1,25 @@
+// Group 03: julius-stabs-back
+// Gautam Ahuja, Nistha Singh
+// Driver function for the parser
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "3_A3.tab.h"
+
+extern void yyerror(char *s);
+
+// We are building Bison Parser
+int main(){
+    // Initialize the parser
+    int parser = yyparse();
+    // If parser returns 0, then the input is valid
+    if(parser == 0){
+        printf("Valid Input\n");
+    }
+    // If parser returns 1, then the input is invalid
+    else{
+        printf("Invalid Input\n");
+    }
+    return 0;    
+}
